713cbee27ccdc6312ef8505cdd8c9a8945cd9a84
Matheus Martins <mathrm@gmail.com>
2022-08-22 11:47:17 +0200

    partial solution

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-const pooledDownload = (connect, save, downloadList, maxConcurrency) => {
-  return connect().then((connection) => {
-    const { download, close } = connection
-    return download(downloadList[0]).then((result) => save(result)) // download the first file and save the result
-  })
+const pooledDownload = async (connect, save, downloadList, maxConcurrency) => {
+    let capacityReached = false;
+    let connections = await Promise.all(
+        [...Array(maxConcurrency).keys()].map(
+            async () => {
+                if (capacityReached) {
+                    return Promise.resolve(null);
+                }
+
+                const connection = await connect();
+
+                if (!connection) {
+                    capacityReached = true;
+                    return null;
+                }
+
+                return connection;
+            }
+        )
+    );
+
+    return await Promise.all(
+        connections.map(
+            connection => executePool(connection, save, downloadList)
+        )
+    )
+}
+
+async function executePool(connection, save, downloadList) {
+    const {download, close} = connection;
+    let result;
+
+    while (downloadList.length > 0) {
+        const itemToDownload = downloadList.shift();
+        result = await download(itemToDownload);
+        await save(result);
+    }
+
+    close();
+
+    return result;
 }
 
 module.exports = pooledDownload
